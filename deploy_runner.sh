46b0c8dfcd5b35b66ee0a383ee8d744ba8d6c3a1
cyberpOOP <vlad0021135@gmail.com>
2026-05-18 23:10:42 +0300

    fix deploy script

--- deploy_runner.sh
+++ deploy_runner.sh
@@ -2,20 +2,22 @@
 set -e
 
 REPO_URL="https://github.com/cyberpOOP/self-improvement"
-RUNNER_TOKEN=$(aws ssm get-parameter --name "/github-runner/token" --with-decryption --query Parameter.Value --output text)
+RUNNER_TOKEN=$(aws ssm get-parameter --name "runner-token" --with-decryption --query Parameter.Value --output text)
 AMI_ID="ami-0122da1d0b4a30ec4" 
 INSTANCE_TYPE="t3.micro"
-IAM_PROFILE="arn:aws:iam::382720393134:user/DevOps"
+IAM_PROFILE="arn:aws:iam::382720393134:instance-profile/DevOps"
+SUBNET_ID="subnet-06c745492d10280f2"
 SG_ID="sg-00bf47e5c03318fce"
 
 USER_DATA=$(cat <<EOF
 #!/bin/bash
-yum update -y
-yum install -y jq curl
+sudo apt update -y
+sudo apt install curl -y
 
 # Install GitHub runner
 mkdir -p /home/ec2-user/actions-runner && cd /home/ec2-user/actions-runner
-curl -sL https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz | tar xz
+curl -o actions-runner-linux-x64-2.334.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.334.0/actions-runner-linux-x64-2.334.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.334.0.tar.gz
 
 # Register runner (ephemeral = de-registers after one job)
 ./config.sh \
@@ -36,6 +38,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
   --image-id "$AMI_ID" \
   --instance-type "$INSTANCE_TYPE" \
   --iam-instance-profile Arn="$IAM_PROFILE" \
+  --subnet-id "$SUBNET_ID"  \
   --security-group-ids "$SG_ID" \
   --user-data "$USER_DATA" \
   --no-associate-public-ip-address \
